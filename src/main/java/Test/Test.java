aa93812190ebffc1cfb0500cd3148082c068d87f
yvake <utyfdfrthby08@gmail.com>
2019-03-07 17:51:51 +0200

    test

---
+++ src/main/java/Test/Test.java
@@ -0,0 +1,12 @@
+package Test;
+
+public class Test {
+    public static void main(String[] args) {
+        int x = 10;
+        if (x != 10){
+            System.out.println("Не равно 10");
+        } else {
+            System.out.println("Равно 10");
+        }
+    }
+}
